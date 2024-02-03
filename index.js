b9ebc4d21fba99d13a7c2202659763f792d63927
Josh Wedekind <josh@halfnibble.com>
2024-02-03 11:57:48 -0800

    Added 404 page

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
     res.send('Hello REST-Rant World!');
 });
 
+// Wildcard/404 route
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>');
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running at http://localhost:${process.env.PORT}`);
 });
