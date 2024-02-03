8e9ac35b223150d82831bd856a6f855292c863e1
Josh Wedekind <josh@halfnibble.com>
2024-02-03 12:19:14 -0800

    Added places controller

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ app.get('/', (req, res) => {
     res.send('Hello REST-Rant World!');
 });
 
+// Load the places controller
+app.use('/places', require('./controllers/places'));
+
 // Wildcard/404 route
 app.get('*', (req, res) => {
+    console.log('user requested unknown route: ', req.url);
     res.status(404).send('<h1>404 Page</h1>');
 });
 
